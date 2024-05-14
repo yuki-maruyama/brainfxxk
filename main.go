cba0e017a474ca665bf46bb0e6fd9c8879c6b5e9
Yuki Maruyama <maruyama_y@1923.co.jp>
2024-05-14 10:49:39 +0900

    Add: read script from file

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main(){
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
+var memory [128]int8
+var cursor int8
+
+func main(){
+	flag.Parse()
+	args := flag.Args()
+
+	var script string
+	if len(args) != 0 {
+		f, err := os.Open(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			script += scanner.Text()
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	
 }
